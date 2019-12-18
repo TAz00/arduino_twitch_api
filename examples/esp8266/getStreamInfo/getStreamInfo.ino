8b1c3a2926c8cafd1edcadbb176305b9d25afc0d
TAz00 <TAz00@users.noreply.github.com>
2019-12-18 19:48:47 +0100

    Added Twitch.tv root certificate
    Connection now works for me, and should work untill 28. Jan 2028
    Changed the code a bit, to load information from multiple channels

--- examples/esp8266/getStreamInfo/getStreamInfo.ino
+++ examples/esp8266/getStreamInfo/getStreamInfo.ino
@@ -8,33 +8,74 @@
 #include <TwitchApi.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-
+#include "CACert.h"
 #include <ArduinoJson.h> // This Sketch doesn't technically need this, but the library does so it must be installed.
 
 //------- Replace the following! ------
-char ssid[] = "SSID";         // your network SSID (name)
+char ssid[] = "ssid";         // your network SSID (name)
 char password[] = "password"; // your network key
 
 // Create a new application on https://dev.twitch.tv/
-#define TWITCH_CLIENT_ID "1234567890654rfsc"
-
-// Username of who you are getting the data for (e.g. "ninja")
-#define TWITCH_LOGIN "brianlough"
+#define TWITCH_CLIENT_ID "tplxyf3ki3rdxdv8c8318dpj1pvslx"
 
 WiFiClientSecure client;
 TwitchApi twitch(client, TWITCH_CLIENT_ID);
 
 unsigned long delayBetweenRequests = 60000; // Time between requests (1 minute)
-unsigned long requestDueTime;               //time when request due
+unsigned long requestDueTime = 0;               //time when request due
 
-void setup()
+unsigned long delayBetweenTimeSync = 3600000; // Time between requests (1 hour)
+unsigned long timesyncDueTime = 0;               
+
+//String twitchNames = "t90official, trentau, itsbudget"
+const int maxNames = 5;
+String twitchNames[maxNames];
+
+void loadEEPROM()
 {
+  //Todo: Actually add EEPROM
+  twitchNames[0] = "t90official";
+  twitchNames[1] = "trentau";
+  twitchNames[2] = "itsbudget";
+  twitchNames[3] = "";
+  twitchNames[4] = "";
+}
 
-    Serial.begin(115200);
+//This is needed to verify certificates
+void setTime()
+{
+  // Synchronize time using SNTP. This is necessary to verify that
+  // the TLS certificates offered by the server are currently valid.
+  Serial.print("Setting time using SNTP");
+  configTime(8 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
 
-    // kss._debug = true;
+// Load root certificate in DER format into WiFiClientSecure object
+void setCertificate()
+{
+    bool res = client.setCACert_P(caCert, caCertLen);
+    if (!res) {
+      Serial.println("Failed to load root CA certificate!");
+      while (true) {
+      yield();
+      }
+    }
+}
 
-    // Set WiFi to station mode and disconnect from an AP if it was Previously
+void setupWifi()
+{
+   // Set WiFi to station mode and disconnect from an AP if it was Previously
     // connected
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
@@ -56,52 +97,82 @@ void setup()
     Serial.println(ip);
 }
 
-void loop()
+void setup()
 {
 
+    Serial.begin(115200);
+
+    //Load EEPROM
+    loadEEPROM();
+    
+    //Init Wifi connection
+    setupWifi();
+    
+    //Set Twitch.tv Root Certificate
+    setCertificate();
+
+    //Set client timeout
+    client.setTimeout(60000);
+}
+
+void loop()
+{
+    //Refresh the clock every hour, not really sure how accurate esp8266 internal clock is
+    if (millis() > timesyncDueTime)
+    {
+      setTime();
+      timesyncDueTime = millis() + delayBetweenTimeSync;
+    }
     if (millis() > requestDueTime)
     {
         Serial.print("Free Heap: ");
         Serial.println(ESP.getFreeHeap());
 
-        Serial.print("Getting Stream info for:");
-        Serial.println(TWITCH_LOGIN);
-        StreamInfo stream = twitch.getStreamInfo(TWITCH_LOGIN);
-        if(!stream.error){
-            Serial.println("---------Stream Info ---------");
-
-            Serial.print("Id: ");
-            Serial.println(stream.id);
-
-            Serial.print("User Id: ");
-            Serial.println(stream.userId);
-
-            Serial.print("User Name: ");
-            Serial.println(stream.userName);
-
-            Serial.print("Game Id: ");
-            Serial.println(stream.gameId);
-
-            Serial.print("Type: ");
-            Serial.println(stream.type);
-
-            Serial.print("Title: ");
-            Serial.println(stream.title);
-
-            Serial.print("Viewer Count: ");
-            Serial.println(stream.viewerCount);
-
-            Serial.print("Started At: ");
-            Serial.println(stream.startedAt);
-
-            Serial.print("Language: ");
-            Serial.println(stream.language);
-
-            Serial.print("Thumbnail URL: ");
-            Serial.println(stream.thumbnailUrl);
-            Serial.println("------------------------");
+        for (int i = 0; i < maxNames; i++) {
+          
+          String twitchName = twitchNames[i];
+          //Skip empty names
+          if(twitchName.length() == 0){continue;}
+          
+          Serial.print("Getting Stream info for: ");
+          Serial.println(twitchName);
+          StreamInfo stream = twitch.getStreamInfo(twitchName.c_str());
+          if(!stream.error){
+              Serial.println("---------Stream Info ---------");
+  
+              Serial.print("Id: ");
+              Serial.println(stream.id);
+  
+              Serial.print("User Id: ");
+              Serial.println(stream.userId);
+  
+              Serial.print("User Name: ");
+              Serial.println(stream.userName);
+  
+              Serial.print("Game Id: ");
+              Serial.println(stream.gameId);
+  
+              Serial.print("Type: ");
+              Serial.println(stream.type);
+  
+              Serial.print("Title: ");
+              Serial.println(stream.title);
+  
+              Serial.print("Viewer Count: ");
+              Serial.println(stream.viewerCount);
+  
+              Serial.print("Started At: ");
+              Serial.println(stream.startedAt);
+  
+              Serial.print("Language: ");
+              Serial.println(stream.language);
+  
+              Serial.print("Thumbnail URL: ");
+              Serial.println(stream.thumbnailUrl);
+              
+          }
+          Serial.println("------------------------");
         }
-
         requestDueTime = millis() + delayBetweenRequests;
     }
 }
